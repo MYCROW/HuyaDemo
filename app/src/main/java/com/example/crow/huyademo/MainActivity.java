4cbf0fe6c3a136bad8647ee8ac01e1878bd78360
MYCROW <853822587@qq.com>
2018-07-02 17:59:16 +0800

    A demo home page(custom view for scrollview)

--- app/src/main/java/com/example/crow/huyademo/MainActivity.java
+++ app/src/main/java/com/example/crow/huyademo/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.crow.huyademo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.crow.huyademo.customview.CategoryTabStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CategoryTabStrip tabs;
+    private ViewPager pager;
+    private MyPagerAdapter adapter;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,6 +52,55 @@ public class MainActivity extends AppCompatActivity {
         //底部选项卡响应
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //顶部选项卡响应
+        tabs = (CategoryTabStrip) findViewById(R.id.category_strip);
+        pager = (ViewPager) findViewById(R.id.view_pager);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
     }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<String> catalogs = new ArrayList<String>();
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            catalogs.add(getString(R.string.top_recommand));
+            catalogs.add(getString(R.string.top_recommand1));
+            catalogs.add(getString(R.string.top_recommand2));
+            catalogs.add(getString(R.string.top_recommand3));
+            catalogs.add(getString(R.string.top_recommand4));
+            catalogs.add(getString(R.string.top_recommand5));
+            catalogs.add(getString(R.string.top_recommand6));
+            catalogs.add(getString(R.string.top_recommand7));
+            catalogs.add(getString(R.string.top_recommand8));
+            catalogs.add(getString(R.string.top_recommand9));
+            catalogs.add(getString(R.string.top_recommand10));
+            catalogs.add(getString(R.string.top_recommand11));
+            catalogs.add(getString(R.string.top_recommand12));
+            catalogs.add(getString(R.string.top_recommand13));
+            catalogs.add(getString(R.string.top_recommand14));
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return catalogs.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return catalogs.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new Fragment();
+        }
+
+    }
+
 
 }
